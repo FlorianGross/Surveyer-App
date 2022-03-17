6cc7311d820ed66c1517ed8aa838fe867b6f249c
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2022-03-17 18:44:03 +0100

    Added Start,User and Admin view

--- app/src/main/java/com/example/surveyer/AdminView.java
+++ app/src/main/java/com/example/surveyer/AdminView.java
@@ -2,16 +2,13 @@ package com.example.surveyer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AdminView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, StartView.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_admin_view);
     }
 }
